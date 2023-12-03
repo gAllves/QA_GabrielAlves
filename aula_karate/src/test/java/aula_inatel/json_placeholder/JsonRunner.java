ab8081343c65889a01ad8a93a7887cd356bb6a11
Alves <gfernandes@ges.inatel.br>
2023-12-03 16:56:08 -0300

    Aula API 3

---
+++ aula_karate/src/test/java/aula_inatel/json_placeholder/JsonRunner.java
@@ -0,0 +1,13 @@
+package aula_inatel.json_placeholder;
+
+import com.intuit.karate.junit5.Karate;
+
+class JsonRunner {
+    
+    @Karate.Test
+    Karate testStarWars() {
+        return Karate.run("json_placeholder").relativeTo(getClass());
+    }    
+
+}
+
